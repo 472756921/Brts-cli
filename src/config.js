c6a0a82f6cb94d4f92477a169a66ac792a192f15
BensonChen <472756921@qq.com>
2021-05-10 10:50:51 +0800

    feat: 新增微前端模板

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const repository = {
 	react: "direct:https://github.com/472756921/Brts.git",
 	react_ts: "direct:https://github.com/472756921/Brts-ts",
+	micro_frontend_template: "direct:https://github.com/472756921/micro_frontend_template",
 };
 exports.repository = repository;
